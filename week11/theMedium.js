78c7d06a0bbe3f7e5409115e1aee2184540007e8
Amanda Martin <amandamartin@Amandas-MacBook-Pro.local>
2020-11-04 16:58:34 -0500

    newest commit

--- week11/theMedium.js
+++ week11/theMedium.js
@@ -1,7 +1,7 @@
 console.log("js!");
 
 let theButton = document.querySelector("button");
-let sites = ["https://www.ap.org/media-center/understanding-the-election", "https://www.nytimes.com/live/2020/11/04/us/election-results", "https://www.cnn.com/politics/live-news/election-results-and-news-11-04-20/index.html", "https://www.washingtonpost.com/elections/2020/11/04/trump-biden-election-live-updates/", "https://www.chicagotribune.com/election-2020/ct-2020-election-results-live-updates-20201103-6z4tvtixefbnbftyfibk25ho5y-story.html"];
+let sites = ["https://www.ap.org/media-center/understanding-the-election", "https://www.nytimes.com/live/2020/11/04/us/election-results", "https://www.cnn.com/politics/live-news/election-results-and-news-11-04-20/index.html", "https://www.washingtonpost.com/elections/2020/11/04/trump-biden-election-live-updates/", "https://www.chicagotribune.com/election-2020/ct-2020-election-results-live-updates-20201103-6z4tvtixefbnbftyfibk25ho5y-story.html", "https://www.nbcnews.com/politics/2020-election/live-blog/2020-11-04-trump-biden-election-results-n1246346/ncrd1246444#liveBlogHeader"];
 // triggering an event with a callback function
 
 theButton.addEventListener('click', theClick);
