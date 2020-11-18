cb50ec92d472b88c906149f578bae4e52bfc4ba1
Amanda Martin <amandamartin@Amandas-MacBook-Pro.local>
2020-11-18 13:20:07 -0500

    newest commit

--- week13/theMedium.js
+++ week13/theMedium.js
@@ -1,15 +1,61 @@
 const milks = ["whole milk", "skim milk", "2% milk", "oat milk", "almond milk", "coconut milk", "soy milk", "no milk", "cream", "foam"];
-const addIns = ["splenda", "sweet n low", "cinnamon", "sugar in the raw", "sugar", "no sugar"];
-const syrups = ["caramel", "vanilla", "mocha", "hazelnut", "no syrup", "pumpkin spice", "white chocolate"];
+const addIns = ["splenda", "sweet n low", "cinnamon", "sugar", "no sugar"];
+const syrups = ["caramel", "vanilla", "mocha", "hazelnut", "no", "pumpkin spice", "white chocolate"];
 const drinks = ["dark roast", "light roast", "medium roast", "americano", "espresso", "macchiato", "latte", "cold brew", "green tea", "black tea", "chai"];
-const temp = ["hot", "iced", "light ice"];
+const temp = ["hot", "iced"];
 
-function order(){
-    const randMilk = milks[Math.floor(Math.random()* milks.length())];
-    const randAddIn = addIns[Math.floor(Math.random()* addIns.length())];
+// function to generate new coffee order each game
+function randOrder(){
+    const randMilk = milks[Math.floor(Math.random()* milks.length)];
+    const randAddIn = addIns[Math.floor(Math.random()* addIns.length)];
     const randSyrup = syrups[Math.floor(Math.random()* syrups.length)];
     const randDrink = drinks[Math.floor(Math.random()* drinks.length)];
     const randTemp = temp[Math.floor(Math.random()* temp.length)];
-    const order = randTemp + ", " + randDrink + ", with " + randMilk + ", " + randAddIn + ", and" + randSyrup;
-    return order;
+    const order = randTemp + " " + randDrink + " with " + randMilk + ", " + randAddIn + ", and " + randSyrup + " syrup";
+    console.log(order);
 }
+function startGame() {
+    $('button').click(function(){
+        window.location.href='game.html';
+     })
+    randOrder();
+    myGameArea.start();
+}
+
+// using each() function to iterate through divs and push id of each div into array $objs
+var $objs = [];
+$('div').each(function(){
+    var id = $(this).attr('id');
+    $objs.push(id);
+});
+console.log($objs);
+
+// selecting random id of divs and storing it in newObj
+function randObj(){
+    const newObj = $objs[Math.floor(Math.random()* $objs.length)];
+    //storing randomly selected img in url string
+    var img = "images/" + newObj + ".jpg";
+    return img;
+}
+
+function generateNew(){
+    // generating new object every 3 seconds
+    setInterval(function(){
+        var myObject = new thing(20, 30, randObj(), 10, 120, "image");
+        }, 3000);
+    
+
+}
+function newImg (width, height, image, x, y, type){
+    $this.css("background-image", img);
+    $this.width() = 20;
+    this.height() = 30;
+
+    
+
+}
+
+
+
+
+
