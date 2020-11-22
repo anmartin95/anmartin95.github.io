63a3f8994707bab68f0c7c449d108417b873ed77
Amanda Martin <amandamartin@Amandas-MacBook-Pro.local>
2020-11-22 17:49:21 -0500

    newest commit

--- week13/theMedium.js
+++ week13/theMedium.js
@@ -1,53 +1,66 @@
 
 // arrays of order options
-const milks = ["whole milk", "skim milk", "2% milk", "oat milk", "almond milk", "coconut milk", "soy milk", "no milk", "cream", "foam"];
+const milks = ["whole milk", "skim milk", "2% milk", "oat milk", "almond milk", "coconut milk", "soy milk", "cream", "foam", "no milk"];
 const addIns = ["splenda", "sweet n low", "cinnamon", "sugar", "no sugar"];
 const syrups = ["caramel", "vanilla", "mocha", "hazelnut", "no", "pumpkin spice", "white chocolate"];
 const drinks = ["dark roast", "light roast", "medium roast", "americano", "espresso", "macchiato", "latte", "cold brew", "green tea", "black tea", "chai"];
 const temp = ["hot", "iced"];
 
-// array of array names
-const types = ["milks", "addIns", "syrups", "drinks", "temp"];
-
 // global vars
 var myOrder;
+var milkNum = Math.floor(Math.random()* milks.length);
+var addInNum = Math.floor(Math.random()* addIns.length);
+var syrupNum = Math.floor(Math.random()* syrups.length);
+var drinkNum = Math.floor(Math.random()* drinks.length);
+var tempNum = Math.floor(Math.random()* temp.length)
 
 // function to generate new coffee order each game
 function randOrder(){
-  const randMilk = milks[Math.floor(Math.random()* milks.length)];
-  const randAddIn = addIns[Math.floor(Math.random()* addIns.length)];
-  const randSyrup = syrups[Math.floor(Math.random()* syrups.length)];
-  const randDrink = drinks[Math.floor(Math.random()* drinks.length)];
-  const randTemp = temp[Math.floor(Math.random()* temp.length)];
+
+  const randMilk = milks[milkNum];
+  const randAddIn = addIns[addInNum];
+  const randSyrup = syrups[syrupNum];
+  const randDrink = drinks[drinkNum];
+  const randTemp = temp[tempNum];
   myOrder = randTemp + " " + randDrink + " with " + randMilk + ", " + randAddIn + ", and " + randSyrup + " syrup";
-  console.log(myOrder);
+  $('.order').text('myOrder');
 }
 
+var $objs = [];
+$('.options').each(function(){
+    var id = $(this).attr('id');
+    $objs.push(id);
+});
+console.log($objs);
+
+//bind buttons on document ready
 $(document).ready(function() {
-  document.write(myOrder);
-  $('.button').click(function() {
-      var spread = $('.canvas').width(); //fill whole canvas
-      setInterval(function(){
-        var type = milks
-        RandomFalling(type, 1, 2, -100, -20, spread, 10, 120, 100, 150, 1400, 1700);
-        }, 3000);
-  });
+    $('.button').click(function() {
+        startGame();
+    });
 });
 
-//start falling elements
-function RandomFalling(classname, num_variations, num_elements, top_pos, left_pos, spread, min_rotation, max_rotation, min_timeout, max_timeout, min_duration, max_duration) 
-{
-	var variation, left_offset, duration, timeout = 0, rotateZ;
-	for (var i=1; i <= num_elements; i++) {
-		variation = Math.floor((Math.random() * num_variations) + 1); //random CSS class
-		left_offset = Math.floor((Math.random() * spread) + left_pos); //set left position
-		duration = Math.floor((Math.random() * (max_duration-min_duration)) + min_duration); //random fall duration
-		rotateZ = Math.floor((Math.random() * (max_rotation-min_rotation)) + min_rotation);	//random rotation angle
-		timeout = timeout + Math.floor((Math.random() * (max_timeout-min_timeout)) + min_timeout);
-		$element = $('<div class="falling-object '+classname+variation+'" style="top: '+top_pos+'px; left: '+left_offset+'px;" />');
+function startGame(){
+	var type = $objs[Math.floor(Math.random() * $objs.length)]; //type of graphic dependent on button clicked
+	var width = $('.canvas').width(); //fill whole canvas
+	fallingImg(type, 6, 100, -100, -20, width, 10, 120, 50, 3000, 3000, 7000);
+}
+
+var variation, className;
+//sprinkle falling elements
+function fallingImg(classname, numVars, numTotal, y, x, spread, minRotate, maxRotate, minStartTime, maxStartTime, minTime, maxTime) {
+	var leftMargin, time, startTime = 0, rotateImg;
+	for (var i=1; i <= numTotal; i++) {
+		className = classname;
+		variation = Math.floor((Math.random() * numVars) + 1); //random CSS img
+		leftMargin = Math.floor((Math.random() * spread) + x); //set left position
+		time = Math.floor((Math.random() * (maxTime-minTime)) + minTime); //random fall duration
+		rotateImg = Math.floor((Math.random() * (maxRotate-minRotate)) + minRotate);	//random rotation angle
+		startTime = startTime + Math.floor((Math.random() * (maxStartTime-minStartTime)) + minStartTime);
+		$element = $('<div class="fallingImg '+classname+variation+'" style="top: '+y+'px; left: '+leftMargin+'px;" />');
 		$element.appendTo('.canvas');
-		$element.delay(timeout).animate({ top: 300, rotateZ: rotateZ}, {
-			duration: duration,
+		$element.delay(startTime).animate({ top: 300, rotateZ: rotateImg}, {
+			duration: time,
 			easing: 'linear',
 			step: function(now, tween) {
 				if (tween.prop === "rotateZ") {
@@ -62,3 +75,9 @@ function RandomFalling(classname, num_variations, num_elements, top_pos, left_po
 		});
 	}
 }
+
+/*function correctOption(){
+	if (className==)
+}
+*/
+
