9ff44a392f0c05135765ab9dd13f58fd91a5efc1
Amanda Martin <amandamartin@Amandas-MacBook-Pro.local>
2020-12-09 22:47:23 -0500

    final project

--- week14/theMedium.js
+++ week14/theMedium.js
@@ -1,5 +1,6 @@
 //global vars
 let songs = ["instrumental", "seven", "betty", "mirrorball", "august", "cardigan"]; // array of song names for string concatenation
+let quotes = [" ", " ", "im only seventeen, I don’t know anything, but I know i miss you"]
 let tempIndex; // var for index of correct song
 var songFile = new Audio();
 
@@ -22,8 +23,17 @@ $("#flipbook").bind("turning", function(event, page, view) {
     tempIndex = (page/2) - 1; // sets the index for songs array to the corresponding page about that song
   else 
     tempIndex = (page-3)/2;
+  displayQuote(tempindex);
   songFile.pause(); // previous song pauses when page starts turning
   var tempSongFile = "audioFiles/" + songs[tempIndex] + ".mp3";
   songFile.src = tempSongFile;
   songFile.play(); // new song plays
 });
+
+function displayQuote(i){
+  $(document).ready(function(){
+    $(".appendedQuotes").append(" <b> " + quotes[i] + "</b> ");
+  });
+};
+
+
